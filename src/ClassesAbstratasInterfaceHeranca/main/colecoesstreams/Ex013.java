d0a68539977a92a6d40d358b663ed1b884f551fa
José Eudes <joseeudes969@gmail.com>
2025-11-05 16:01:53 -0300

    Ex013

---
+++ src/ClassesAbstratasInterfaceHeranca/main/colecoesstreams/Ex013.java
@@ -0,0 +1,16 @@
+package ClassesAbstratasInterfaceHeranca.main.colecoesstreams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex013 {
+    public static void main(String[] args) {
+        List<String> funcionarios =  new ArrayList<>();
+        funcionarios.add("João");
+        funcionarios.add("Maria");
+        funcionarios.add("Vitor");
+        funcionarios.add("Ana");
+
+        System.out.println(funcionarios);
+    }
+}
